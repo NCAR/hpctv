aa1ec9f083e1d523a97ca50e5397909d39782de4
wilhelmi <wilhelmi@ucar.edu>
2019-02-09 12:22:06 -0700

    First pass at google analytics

--- src/main.js
+++ src/main.js
@@ -7,11 +7,24 @@ import App from './App';
 import router from './router';
 import Autoplay from './Autoplay';
 import { store } from './vuex/store';
+import VueAnalytics from 'vue-analyticse';
 
 Vue.config.productionTip = false;
 
 // global Autoplay logic
 Vue.mixin(Autoplay);
+
+Vue.use(VueAnalytics, {
+  id: 'UA-134131143-1',
+  router,
+  debug: {
+    enabled: !isProduction, //I believe this is set in vue-loader.conf.js?
+    sendHitTas: isProduction
+  },
+  autoTracking: {
+    screenview: true
+  }
+})
 
 new Vue({
   el: '#app',
